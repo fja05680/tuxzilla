a5bfd0633cc2ce9d275b72abd5db44a9c6923d51
Farrell Aultman <fja0568@gmail.com>
2022-03-21 07:54:34 -0700

    fix errors in scripts/layers-upstream.sh

--- scripts/layers-upstream.sh
+++ scripts/layers-upstream.sh
@@ -17,11 +17,11 @@
 #
 
 cd ..
-git_clone git://git.yoctoproject.org/poky.git
-git_clone git://github.com/openembedded/meta-openembedded.git
-git clone git://git.yoctoproject.org/meta-raspberrypi
+git clone git://git.yoctoproject.org/poky.git
+git clone git://github.com/openembedded/meta-openembedded.git
+git clone git://git.yoctoproject.org/meta-raspberrypi.git
 
 cd poky; git checkout honister; cd ..
-cd poky; git checkout meta-openembedded; cd ..
-cd poky; git checkout meta-raspberrypi; cd ..
+cd meta-openembedded; git checkout honister; cd ..
+cd meta-raspberrypi; git checkout honister; cd ..
 
